5a79e42725b0bd162e8987c12908272add4c0a54
Chir@g159 <dchirag159@gmail.com>
2024-08-11 16:07:23 +0530

    Pass TestCase 1, Add read_data() function

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -1,2 +1,25 @@
 import pandas as pd
 
+DATA_FILE_PATH = "data/books.csv"
+
+
+def read_data() -> pd.DataFrame:
+    '''
+    Reads data from csv file.
+
+    Returns
+    -------
+    Dataframe
+            Data in Dataframe format.
+    '''
+
+    try:
+        books_df = pd.read_csv(DATA_FILE_PATH)
+    except pd.errors.EmptyDataError:
+        pass
+    except Exception:
+        pass
+    else:
+        return books_df
+    
+    return
